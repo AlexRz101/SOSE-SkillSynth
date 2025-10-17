6a035091a18f922c9604496267c16b6d44d2fc65
Alex <Prestge.alex101@gmail.com>
2025-10-17 14:12:32 -0700

    Added autocomplete drop menu with some cs topics

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,13 +3,14 @@ import Header from './Header.tsx';
 import Footer from './Footer.tsx';
 //import Body from './Body.tsx';
 import HideableText from './HideableText.tsx';
+import AutoCompleteText from './AutoCompleteText.tsx';
 
 function App() {
 
   return (
     <>
       <Header/>
-      <HideableText text=" "/>
+      <AutoCompleteText text='Array stuff'/>
       <Footer/>
     </>
   )
