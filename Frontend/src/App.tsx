300f51ddad9f390e6ae5864d7d46de1ff8ef51cc
SeanRajM <sean-raj.madanu.976@my.csun.edu>
2025-10-19 00:25:44 -0700

    Added ML -> API -> FE

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
-import Header from './Header.tsx';
-import Footer from './Footer.tsx';
-//import Body from './Body.tsx';
+import Pages from './Pages.tsx'
 
 function App() {
-
   return (
-    <>
-      <Header/>
-      <Footer/>
-    </>
-  )
+    <Pages/>
+  );
 }
 
-export default App
+export default App;
